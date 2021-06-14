b5692e69082fedfde662ab018939d6d793848b81
ldh2003 <80668146+ldh2003@users.noreply.github.com>
2021-06-14 14:41:20 +0900

    0614
    .

--- Assets/Scripts/Enermy.cs
+++ Assets/Scripts/Enermy.cs
@@ -8,10 +8,14 @@ public class Enermy : MonoBehaviour
     public Transform target;
     private Rigidbody rig;
     public float speed = 20f;
+
+    public AudioSource enermyAudio; 
+    public GameObject explosionEffect;
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
         target = GameObject.FindWithTag("Player").transform;
+        enermyAudio = GetComponent<AudioSource>();  
     }
     void Update()
     {
@@ -24,11 +28,14 @@ public class Enermy : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     { 
         if (other.tag == "Bullet" && scored == false)
-        { 
+        {
+            enermyAudio.Play();
+            Instantiate(explosionEffect, transform.position, transform.rotation);
             scored = true;
             GameManager.instance.AddScore(1);
             transform.position = new Vector3(Random.Range(-30, 30), 0.5f, Random.Range(-30, 30));
-            scored = false; 
+            scored = false;
+           
         } 
     }
 
